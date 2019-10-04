cce2b8bfff055b998ba853939a8ee7d5fc25f6d4
Patrick Kennedy <pkennedytx1@gmail.com>
2019-10-04 10:53:19 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
+  created_table "school_classes", forcee :cascade do |t|
+    t.string  "title"
+    t.integer "room_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
